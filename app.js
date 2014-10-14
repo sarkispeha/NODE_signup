183d76099b8a682b60ec7c39e644e11813834c3b
Sarkis Peha <sarkispeha@gmail.com>
2014-10-14 16:51:20 -0600

    completed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express();
@@ -5,8 +6,24 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
-	res.render('index');
+	fs.readFile('data.txt', function(err, data){
+		res.header('Content-Type', 'text/html');
+		res.send(data);
+	})
 });
+
+app.post('/formsubmit', function(req, res) {
+	res.redirect('/success');
+	res.send(req.body);
+	console.log(req.body);
+	// fs.readFile('', function(err, data){	
+	// })
+});
+
+app.get('/success', function(req, res){
+	res.send('Heyoooooo!')
+})
+
 
 var server = app.listen(8111, function() {
 	console.log('Express server listening on port ' + server.address().port);
